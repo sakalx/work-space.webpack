1b27919bb0a34f44398eaeea4cb4615d2828d16d
sakals <sakals@mail.ua>
2017-09-11 23:24:44 -0500

    burn all
    Signed-off-by: sakals <sakals@mail.ua>

--- src/js/share/components/jsx/share.vie.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import {Aside} from './components.jsx/Aside.jsx';
-
-const aside = document.getElementById('aside');
-ReactDOM.render(<Aside/>, aside);
