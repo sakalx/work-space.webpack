e6b6839328c15108ede715c888268aac043e1c12
sakals <sakals@mail.ua>
2017-09-21 21:06:55 -0500

    Redux part II -> ajax
    Signed-off-by: sakals <sakals@mail.ua>

--- src_redax/js/glob.js
+++ src_redax/js/glob.js
@@ -15,7 +15,6 @@ import muiTheme from './helpers/muiTheme/colors.js';
 //Glob App
 //import App from './app/App.jsx';
 
-
 /* injectTapEventPlugin();
  ReactDOM.render(
  <MuiThemeProvider muiTheme={getMuiTheme(muiTheme)}>
@@ -25,14 +24,111 @@ import muiTheme from './helpers/muiTheme/colors.js';
 
 
 //Redux
-import {addNumber, substractNumber} from './app/actions/mathActions';
-import {setName, setAge}from './app/actions/userActions';
-import store from './app/store';
+// import {addNumber, substractNumber} from './app/actions/mathActions';
+// import {setName, setAge}from './app/actions/userActions';
+// import store from './app/store';
+//
+// store.dispatch(addNumber(10));
+// store.dispatch(addNumber(100));
+// store.dispatch(substractNumber(50));
+//
+// store.dispatch(setName('Felix'));
+// store.dispatch(setAge(18));
 
-store.dispatch(addNumber(10));
-store.dispatch(addNumber(100));
-store.dispatch(substractNumber(50));
+import thunk from 'redux-thunk'; //for async task
+import axios from 'axios';       //for ajax reg
+// combined with redux-thunk to chain async actions
+import promise from 'redux-promise-middleware'
+import logger from 'redux-logger';
+import {createStore, combineReducers, applyMiddleware} from 'redux';
 
-store.dispatch(setName('Felix'));
-store.dispatch(setAge(18));
+const userReducer = (state = {}, action) => {
+  switch (action.type) {
+    case 'CHANGE_NAME': {
+      state = {
+        ...state,
+        name: action.payload,
+      };
+      break;
+    }
+    case 'CHANGE_AGE': {
+      state = {
+        ...state,
+        age: action.payload,
+      };
+      break;
+    }
+  }
+  return state;
+};
+
+//default state for reduserFetch
+const initialState ={
+  fetching: false,
+  fetched: false,
+  users: [],
+  error: null,
+};
+const reduserFetch = (state = initialState, action) => {
+  switch (action.type) {
+    case 'FETCH_USER_PENDING': {
+      return {
+          ...state,
+        fetching: true
+      };
+    }
+    case 'FETCH_USERS_FULFILLED': {
+      return {
+        ...state,
+        fetching:false,
+        fetched: true,
+        users: action.payload,
+      };
+    }
+    case 'FETCH_USER_REJECTED': {
+      return {
+        ...state,
+        fetching: false,
+        error: action.payload,
+      };
+    }
+  }
+  return state;
+};
+
+
+// В кучку all Reducers
+const reducers = combineReducers({
+  userReducer,
+  reduserFetch,
+});
+
+//Промежуточное сосотояние
+//используем пакет redux-logger для консоля
+//thunk - для асинхроной загрузки
+const middleware = applyMiddleware(promise(), thunk, logger);
+
+//Создаем Хранилище
+const store = createStore(
+    reducers,
+    middleware,
+);
+
+//Отправка события в Хранилище
+store.dispatch({
+  type: 'CHANGE_NAME',
+  payload: 'Felix',
+});
+
+store.dispatch({
+  type: 'CHANGE_AGE',
+  payload: 17,
+});
+
+//AJAX with axios and redux-promise-middleware
+store.dispatch({
+  type: 'FETCH_USERS',
+  payload: axios.get('https://jsonplaceholder.typicode.com/users/')
+});
+
 
