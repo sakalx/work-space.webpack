6d34f7dcea9d2bae859a50b1aad5e68f19c0dc12
sakals <sakals@mail.ua>
2017-09-21 21:05:11 -0500

    housekeeping
    Signed-off-by: sakals <sakals@mail.ua>

--- src_Router/js/glob/glob.js
+++ src_Router/js/glob/glob.js
@@ -1,22 +1,5 @@
 import 'root/style/glob/glob.scss'
-import $ from 'jquery';
+
 
 //Glob Glob Layout
 import './client/glob.Layout.jsx';
-
-
-//Glob Functional
-import F__initInputfield from './functional/input.js';
-import F__initRipples from './functional/ripples.js'
-
-F__initInputfield();
-
-$('button').click(function() {
-  F__initRipples();
-});
-
-
-$('input').hover(function() {
-  console.log('hover');
-  F__initInputfield();
-});
